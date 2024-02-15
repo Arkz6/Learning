c7f58ed065d11b1bd4e47bc53be2e8f938691ca5
Arkz6 <159614365+Arkz6@users.noreply.github.com>
2024-02-15 21:31:36 +0100

    Add files via upload

---
+++ input.py
@@ -0,0 +1,13 @@
+imię = input('Podaj swoje imię: ')
+#print('Cześć', imię)
+print('Cześć {}!' .format(imię))
+
+# %%
+jezyk = input('Jakiego języka się uczysz {}?' .format(imię))
+print('Podano {}' .format(jezyk))
+
+# %%
+name = input('Podaj swoje imię: ')
+age = int(input('Podaj swój wiek: '))
+print('Cześć {}! Masz {} lat' .format(name, age))
+input()
