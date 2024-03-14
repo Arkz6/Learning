51c2da21f9b20a98e817e29809d3d1a6465cc649
Arkz6 <159614365+Arkz6@users.noreply.github.com>
2024-03-14 20:53:55 +0100

    Add files via upload

--- listy.py
+++ listy.py
@@ -2,4 +2,29 @@
 techs = ['python', 'java', 'sql', 'c++']
 techs[0] = 'python 3.12'
 print(techs)
+
+# %%
+numbers = [1, 2, 3, 4, 5, 6]
+print(numbers)
+
+# %%
+empty = []
+nested = [[1, 2, 3, ['arek', 'tomek', 'karol']], ['python', 'sql'], 8]
+
+# %%
+first = ['mleko', 'woda', 'piwo']
+second = ['jajka', 'chleb']
+bucket = [first, second]
+print(bucket)
+
+# %%
+len(bucket)
+
+# %%
+first + second
+
+# %%
+first += ['paluszki']
+print(first)
+
 # %%
